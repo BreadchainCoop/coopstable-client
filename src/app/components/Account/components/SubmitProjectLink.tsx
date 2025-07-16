a233c15e22802ef2c089019184421a6c05a575e7
RasGUY <ftrouw@protonmail.com>
2025-07-16 23:19:02 +0530

    feat: update submit project link

--- src/app/components/Account/components/SubmitProjectLink.tsx
+++ src/app/components/Account/components/SubmitProjectLink.tsx
@@ -4,7 +4,7 @@ export function SubmitProjectLink() {
   return (
     <Link 
     target="_blank" 
-    href="https://form.typeform.com/to/sHp99h2H" 
+    href="https://ruben280.typeform.com/to/NTdqRDcK" 
     className="hidden md:inline mx-1 font-theme-2 px-6 py-3 text-[20px] lg:text-xl text-theme-black bg-theme-grey-3 font-bold uppercase hover:cursor-pointer"
     >Submit Project</Link>
   );
