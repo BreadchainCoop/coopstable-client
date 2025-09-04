7c95c2e98b18a668b501706147c2da57fc8f2ce4
RasGUY <ftrouw@protonmail.com>
2025-09-04 15:31:03 +0530

    feat: remove dead member

--- src/app/components/MemberDistributions/constants.ts
+++ src/app/components/MemberDistributions/constants.ts
@@ -4,17 +4,9 @@ export const MEMBER_INFO: MemberInfo[] = [
     {
         name: "Phoenix DeFi Hub",
         description: "The bext DEX on Stellar built with Soroban",
-        treasury: "GCNPDMUMRXBFHMM7KSXU3KVYNM73EDHBFMDK4O33HPY7DA2LXZOZQRDB",
+        treasury: "GAPRPZYCIV3QPMCTWSRDNY64EJMZNCJFUCTJHQDQNW6RJ66TEVEH5UDU",
         shareOfYield: "4000.45",
         percentageOfYield: "15.5",
         website: "https://www.phoenix-hub.io/"
-    },
-    {
-        name: "Dustaggregator",
-        description: "A Dust Aggregator is a blockchain tool or smart contract that helps users collect and convert tiny token balances (called \"dust\") spread across multiple tokens or wallets. These small amounts are usually too small to trade or transfer efficiently due to gas fees or minimum swap limits.",
-        treasury: "GCQSQHJBSQUFHOR7ZV6GK6T4KJFNZ2HB7TRSLPHNRU5XT2OHJA5CKZOU",
-        shareOfYield: "2000.55",
-        percentageOfYield: "17.9",
-        website: "https://dustaggregator.vercel.app/"
-    },
+    }
 ]
